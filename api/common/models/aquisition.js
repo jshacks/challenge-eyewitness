ae721280f5f7ea7ba02e67d07f84650852947cf6
maximblack <maximblack@gmail.com>
2016-10-22 21:19:06 +0300

    aquisitions api side

--- api/common/models/aquisition.js
+++ api/common/models/aquisition.js
@@ -2,32 +2,128 @@
 
 var request = require('request');
 
+var fs = require('fs');
+var parse = require('csv-parse');
+var transform = require('stream-transform');
+
+var loopback = require('loopback');
+
 var TENDER_LIST_URL = 'http://etender.gov.md/json/tenderList';
 
 module.exports = function(Aquisition) {
 
+    Aquisition.geoPopulate = function(cb) {
+
+        var collection = Aquisition.getDataSource().connector.collection(Aquisition.modelName);
+        //var entriesCursor = collection.find({});
+
+
+        Aquisition.find({}, function(err, entries) {
+
+            //console.log('entries length', entries.length);
+
+            var iterator = 0;
+
+            var parser = parse({delimiter: ','});
+            var input = fs.createReadStream(__dirname + '/../../mock/geo.csv');
+
+            var transformer = transform(function(record, callback){
+
+                var lat = record[1];
+                var long  = record[3];
+
+                var geoPoint = new loopback.GeoPoint({lat: lat, lng: long});
+
+
+                var entry = entries[iterator++];
+
+                if(entry === undefined) return callback();
+
+                collection.update({
+                    _id: entry.id
+                },
+                    {
+                        $set: {
+                            location: geoPoint
+                        }
+                    },
+                {
+                    //upsert: true
+                }, function() {
+
+                });
+
+                return callback();
+
+
+            }, {
+
+            });
+
+            input.pipe(parser).pipe(transformer);
+        });
+
+
+
+    };
+
   Aquisition.cpv = function(cb) {
 
-    var ds = Aquisition.app.datasources.db;
+      var collection = Aquisition.getDataSource().connector.collection(Aquisition.modelName);
+      collection.distinct('data.tenderData.goods', function (err, result) {
+          result = result.map(function (entry) {
+              return {
+                  code: entry.code,
+                  name: entry.mdValue
+              };
+          });
+          cb(null, result);
+      });
 
-    /*ds.connector.db.distinct('tenderId', function (err, cpvs) {
-      console.log(cpvs);
-    });
-    console.log(ds);*/
+  };
 
-  }
+    Aquisition.statuses = function(cb) {
 
-  Aquisition.top = function(limit, cb) {
+        var collection = Aquisition.getDataSource().connector.collection(Aquisition.modelName);
+        collection.distinct('data.tenderStatus', function (err, result) {
+            result = result.map(function (entry) {
+                return {
+                    id: entry.id,
+                    name: entry.mdValue
+                };
+            });
+            cb(null, result);
+        });
+
+    };
+
+
+  Aquisition.top = function(filters, limit, skip, cb) {
+
+      var where = {};
+
+      if(filters['statusId']) {
+          where['data.tenderStatus.id'] = filters['statusId'];
+      }
+
+      if(filters['cpv']) {
+          where['data.tenderData.goods.code'] = filters['cpv'];
+      }
 
     Aquisition.find({
-      limit: limit
+      limit: limit || 100,
+      skip: skip || 0,
+      where: where
     }, function(err, acquisitions) {
         acquisitions = acquisitions.map(function(entry) {
 
           var entryData = entry.data;
 
           return {
-            id: entry.tenderId,
+            id: entry.id,
+            tenderId: entry.tenderId,
+            location: entry.location,
+            status: entryData.tenderStatus.mdValue,
             type: entryData.tenderType.mdValue,
             description: entryData.tenderData.goodsDesacr,
             open_date: entryData.refTendeOpenDate,
@@ -97,6 +193,14 @@ module.exports = function(Aquisition) {
       }
     }
 
+    Aquisition.remoteMethod(
+        'geoPopulate',
+        {
+            accepts: null,
+            returns: {arg: 'status', type: 'boolean'}
+        }
+    );
+
   Aquisition.remoteMethod(
     'cpv',
     {
@@ -105,10 +209,22 @@ module.exports = function(Aquisition) {
     }
   );
 
+    Aquisition.remoteMethod(
+        'statuses',
+        {
+            accepts: null,
+            returns: {arg: 'statuses', type: 'array'}
+        }
+    );
+
   Aquisition.remoteMethod(
     'top',
     {
-      accepts: {arg: 'limit', type: 'number'},
+      accepts: [
+          {arg: 'filters', type: 'json'},
+          {arg: 'limit', type: 'number'},
+          {arg: 'skip', type: 'number'}
+      ],
       returns: {arg: 'aquisitions', type: 'array'}
     }
   );
